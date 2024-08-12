585eed0d5c939326857b7533ef246e2e3481e3df
tomasesber <tomas.esber@duke.edu>
2024-08-12 10:13:57 -0400

    Added functionality in League.jsx to start trying to make the leaderboard. Display predictions for previous matchdays for all users in a league, and for current matchday if and only if the matchday is locked (the first game has started).

--- client/src/League.jsx
+++ client/src/League.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { db } from './firebase';
-import { collection, doc, getDoc } from 'firebase/firestore';
-
+import { collection, doc, getDoc, query, where, getDocs } from 'firebase/firestore';
+import axios from 'axios';
 
 const League = () => {
     const { leagueId } = useParams();
     const [league, setLeague] = useState(null);
+    const [currentMatchday, setCurrentMatchday] = useState('');
+    const [predictions, setPredictions] = useState([]);
 
+    /**
+     * useEffect is a React Hook that lets you synchronize a component with an external system.
+     */
     useEffect(() => {
         const fetchLeague = async () => {
             const leagueCollection = collection(db, 'leagues')
@@ -17,8 +22,86 @@ const League = () => {
                 setLeague(leagueDoc.data());
             }
         };
+
+        /**
+        * Fetches the current matchday from the backend. Also reused from MakePredictions. Refactor in future.
+        */
+        const fetchCurrentMatchdayFromDatabase = async () => {
+            const matchdayDoc = await getDoc(doc(db, 'metadata', 'currentMatchday'));
+            if (matchdayDoc.exists()) {
+                setCurrentMatchday(matchdayDoc.data().value);
+            } else {
+                try {
+                    const response = await axios.get('http://localhost:3000/fetchCurrentMatchday');
+                    setCurrentMatchday(response.data.currentMatchday);
+                } catch (error) {
+                    console.error('Error fetching current matchday:', error);
+                }
+            }
+        };
+
         fetchLeague();
+        fetchCurrentMatchdayFromDatabase();
+        // list of dependencies for the useEffect hook
     }, [leagueId]);
+
+    useEffect(() => {
+        if (currentMatchday !== null) {
+            fetchUsersPredictions();
+        }
+    }, [currentMatchday]);
+
+    /**
+     * Helper function to check if predictions should be locked according to our current matchday
+     * Note: this is a repeated function. Refactor it to a utility function
+     * @param {*} matchday the current matchday
+     * @returns true if predictions should be locked, false otherwise
+     */
+    const shouldLockPredictions = async (matchday) => {
+        try {
+            // Get the matches for the current matchday
+            const matchesRef = collection(db, 'matches');
+            const currentMatchdayQuery = query(matchesRef, where('matchday', '==', matchday));
+            const currentMatchdaySnapshot = await getDocs(currentMatchdayQuery);
+            
+            // sort the matches by start time and get the first match
+            const currentTime = new Date();
+            const firstMatchStartTime = currentMatchdaySnapshot.docs
+                .map(doc => new Date(doc.data().utcDate))
+                .sort((a, b) => a - b)[0];
+            // check if the current time is greater than the first match start time
+            return currentTime >= firstMatchStartTime;
+        } catch (error) {
+            console.error('Error checking if predictions should be locked:', error);
+            return true;
+        }
+    };
+
+    /**
+     * Fetches the predictions for the current and previous matchdays from the database
+     */
+    const fetchUsersPredictions = async () => {
+        // Get the predictions collection
+        const predictionsCollection = collection(db, 'predictions');
+        // Create a query to get the predictions for previous and current matchdays
+        const q = query(predictionsCollection, where('leagueId', '==', leagueId), where('matchday', '<=', currentMatchday));
+
+        // Get the predictions for the current matchday
+        const querySnapshot = await getDocs(q);
+        // Get the data from the query snapshot
+        const predictionsData = querySnapshot.docs.map(doc => doc.data());
+
+        // Filter the predictions to only show the predictions for the current matchday if the matchday is locked
+        const filteredPredictions = predictionsData.filter(prediction => {
+            if (prediction.matchday === currentMatchday) {
+                // console.log('currentMatchday:', currentMatchday);
+                // console.log('shouldLockPredictions:', shouldLockPredictions(currentMatchday));
+                return shouldLockPredictions(currentMatchday);
+            }
+            return false;
+        });
+        setPredictions(filteredPredictions);
+    };
 
     return (
         <div>
@@ -28,6 +111,15 @@ const League = () => {
                     <h3>{league.name}</h3>
                     <p>{league.description}</p>
                     <Link to={`/makePredictions/${leagueId}`}>Make Predictions</Link>
+
+                    <h4>Predictions</h4>
+                    <ul>
+                        {predictions.map((prediction, index) => (
+                            <li key={index}>
+                                User: {prediction.userId}, Matchday: {prediction.matchday}, Team: {prediction.teamId}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             ) : (
                 <p>Loading...</p>
